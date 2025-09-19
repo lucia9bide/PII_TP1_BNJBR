d73963872f52eb6abed2722b83f21533d1f5fe87
I. M. Napal <imnapal2400@gmail.com>
2025-09-19 02:05:04 +0000

    Update hamb-btn.js

--- js/hamb-btn.js
+++ js/hamb-btn.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const hamburguesaBtn = document.getElementById("hamburguesa");
     const navMenu = document.getElementById("nav");
 
-    hamburgerBtn.addEventListener('click', function() {
+    hamburguesaBtn.addEventListener('click', function() {
         
         navMenu.classList.toggle('active');
             });
