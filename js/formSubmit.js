950897648337dbf76b24233c64e01d3f60351025
I. M. Napal <imnapal2400@gmail.com>
2025-09-19 01:22:56 +0000

    Update formSubmit.js

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -1,20 +1,19 @@
-var form = getElementById("formulario")
-var consulta.nombre = getElementById("firstname").value
-consulta.apellido   = getElementById("lastname").value
-consulta.pais       = getElementById("country").value
-consulta.tipoDoc    = getElementById("identificacion").value === "1" ? "dni" : "pasaporte"
-consulta.numDoc     = getElementById("documentacion").value
-consulta.edad       = getElementById("edad").value
-// !!!! hay que agregar el id="edad" a esa parte de formulario.html!!! Si no, no lo va a agarrar.
-consulta.email      = getElementById("mail").value
-consulta.tel        = getElementById("numberTel").value
-consulta.genero     = getElementById("genderbox").value
-consulta.tipoConsul = getElementById("consultabox").value
-consulta.textoConsul= getElementById("comentario").value
-// !!!! hay que agregar el id="comentario" a esa parte de formulario.html!!! Si no, no lo va a agarrar.
 
-function sendToServer(event)
+async function sendToServer(event)
 {
+    var form = document.getElementById("formulario");
+    var consulta;
+    consulta.nombre = getElementById("firstname").value
+    consulta.apellido   = getElementById("lastname").value
+    consulta.pais       = getElementById("country").value
+    consulta.tipoDoc    = getElementById("identificacion").value === "1" ? "dni" : "pasaporte"
+    consulta.numDoc     = getElementById("documentacion").value
+    consulta.edad       = getElementById("edad").value
+    consulta.email      = getElementById("mail").value
+    consulta.tel        = getElementById("numberTel").value
+    consulta.genero     = getElementById("genderbox").value
+    consulta.tipoConsul = getElementById("consultabox").value
+    consulta.textoConsul= getElementById("comentario").value
     event.preventDefault();
     var conjson = JSON.stringify(consulta)
     const response = await fetch('http://localhost:3000/submission', {
@@ -27,4 +26,3 @@ function sendToServer(event)
 }
 
 form.addEventListener("submit", sendToServer)
-// !!!! hay que agregar el id="btn-enviar" a esa parte de formulario.html!!! Si no, no lo va a agarrar.
